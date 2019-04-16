2c9e299d10250d5d5ab79f388b5e30c47e0f5400
Jan Lipnican <yanlipnican@gmail.com>
2019-04-16 14:31:24 +0200

    lol

--- src/App.js
+++ src/App.js
@@ -1,101 +1,159 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-import {ColumnChart, PivotTable} from '@gooddata/react-components';
 import '@gooddata/react-components/styles/css/main.css';
-
-const m1 = {
-    "measure": {
-        "localIdentifier": "m1",
-        "definition": {"measureDefinition": {"item": {"uri": "/gdc/md/ux8xk21n3al4qr1akoz7j6xkl5dt1dqj/obj/1279"}}}
-    }
-};
-
-const m2 = {
-    "measure": {
-        "localIdentifier": "m2",
-            "definition": {"measureDefinition": {"item": {"uri": "/gdc/md/ux8xk21n3al4qr1akoz7j6xkl5dt1dqj/obj/1283"}}}
-    }
-}
-
-const a1 = {
-    "visualizationAttribute": {
-        "displayForm": {"uri": "/gdc/md/ux8xk21n3al4qr1akoz7j6xkl5dt1dqj/obj/1027"},
-        "localIdentifier": "a1"
-    }
-};
-
-const a2 = {
-    "visualizationAttribute": {
-        "displayForm": {"uri": "/gdc/md/ux8xk21n3al4qr1akoz7j6xkl5dt1dqj/obj/952"},
-        "localIdentifier": "a2"
-    }
-};
-
-const a3 = {
-    "visualizationAttribute": {
-        "displayForm": {"uri": "/gdc/md/ux8xk21n3al4qr1akoz7j6xkl5dt1dqj/obj/1028"},
-        "localIdentifier": "a3"
-    }
-}
-
-const total_a1_m1 = {
-    "measureIdentifier":"m1",
-    "type":"sum",
-    "attributeIdentifier":"a1"
-};
-
-const total_a2_m1_sum = {
-    "measureIdentifier":"m1",
-    "type":"sum",
-    "attributeIdentifier":"a2"
-};
-
-const total_a2_m2_sum = {
-    "measureIdentifier":"m2",
-    "type":"sum",
-    "attributeIdentifier":"a2"
-};
-
-const total_a2_m2_max = {
-    "measureIdentifier":"m2",
-    "type":"max",
-    "attributeIdentifier":"a2"
-};
-
-const total_a3_m2_sum = {
-    "measureIdentifier":"m2",
-    "type":"sum",
-    "attributeIdentifier":"a3"
-};
-
-const total_a3_m2_max = {
-    "measureIdentifier":"m2",
-    "type":"max",
-    "attributeIdentifier":"a3"
-};
+import { PivotTable } from '@gooddata/react-components';
 
 class App extends Component {
+    ref = null;
+
+    componentDidMount() {
+        this.ref.addEventListener('drill', (evt) => {
+            console.log('drill event', evt.detail);
+        });
+    }
+
+    onDrill = (drillEvent) => {
+        console.log('onFiredDrillEvent', drillEvent);
+        return true;
+    };
+
     render() {
+        // const projectId = 'ux8xk21n3al4qr1akoz7j6xkl5dt1dqj';
+        const projectId = 'hzvp93huecapm943k1j0sncbdxeug4gv';
+
+        const measures = [{
+            measure: {
+                localIdentifier: 'm1',
+                definition: {
+                    measureDefinition: {
+                        item: {
+                            uri: `/gdc/md/${projectId}/obj/9211`
+                        }
+                    }
+                },
+                title: 'm1'
+            }
+        }, {
+            measure: {
+                localIdentifier: 'm2',
+                definition: {
+                    measureDefinition: {
+                        item: {
+                            uri: `/gdc/md/${projectId}/obj/9203`
+                        }
+                    }
+                },
+                title: 'm2'
+            }
+        }];
+
+        const rows = [{
+            visualizationAttribute: {
+                displayForm: {
+                    uri: `/gdc/md/${projectId}/obj/1027`
+                },
+                localIdentifier: "a1"
+            }
+        }, {
+            visualizationAttribute: {
+                displayForm: {
+                    uri: `/gdc/md/${projectId}/obj/64727`
+                },
+                localIdentifier: "a2"
+            }
+        }, {
+            visualizationAttribute: {
+                displayForm: {
+                    uri: `/gdc/md/${projectId}/obj/1024`
+                },
+                localIdentifier: "a3"
+            }
+        }, {
+            visualizationAttribute: {
+                displayForm: {
+                    uri: `/gdc/md/${projectId}/obj/1094`
+                },
+                localIdentifier: "a4"
+            }
+        }];
+
+        const columns = [{
+            visualizationAttribute: {
+                displayForm: {
+                    uri: `/gdc/md/${projectId}/obj/324`
+                },
+                localIdentifier: "a5"
+            }
+        }];
+
+        const totals = [{
+            type: 'sum',
+            measureIdentifier: 'm1',
+            attributeIdentifier: 'a1'
+        }, {
+            type: 'max',
+            measureIdentifier: 'm1',
+            attributeIdentifier: 'a1'
+        }, {
+            type: 'sum',
+            measureIdentifier: 'm1',
+            attributeIdentifier: 'a2'
+        }, {
+            type: 'min',
+            measureIdentifier: 'm1',
+            attributeIdentifier: 'a4'
+        }, {
+            type: 'avg',
+            measureIdentifier: 'm1',
+            attributeIdentifier: 'a4'
+        }];
+
+        const sortBy = [{
+            attributeSortItem: {
+                direction: 'asc',
+                attributeIdentifier: 'a1'
+            }
+        }];
+
+        const drillableItems = [/*
+            HeaderPredicateFactory.localIdentifierMatch('a1'),
+            HeaderPredicateFactory.localIdentifierMatch('a2'),
+            HeaderPredicateFactory.localIdentifierMatch('a3'),
+            HeaderPredicateFactory.localIdentifierMatch('m1')
+        */];
+
         return (
-            <div className="App">
-                <div style={{height: 400, padding: 100}}>
-                    <PivotTable
-                        projectId="ux8xk21n3al4qr1akoz7j6xkl5dt1dqj"
-                        measures={[m1, m2]}
-                        rows={[a1, a2, a3]}
-                        totals={[total_a1_m1, total_a2_m2_sum, total_a2_m2_max, total_a3_m2_sum, total_a3_m2_max]}
-                        groupRows={true}
-                    />
+            <div className="App" ref={ref => this.ref = ref}>
+
+                <div className="App-header">
+                    <h1>GoodData SDK Playground</h1>
+                    <a href="/account.html" className="login-link">Login first to see the data</a>
                 </div>
-                <div style={{height: 400, padding: 100}}>
-                    <ColumnChart
-                        projectId="ux8xk21n3al4qr1akoz7j6xkl5dt1dqj"
-                        measures={[m1]}
-                        viewBy={a2}
-                    />
+
+                <div className="insight-wrapper">
+
+                    <div style={{ height: 800 }} className="s-pivot-table-sorting">
+                        <PivotTable
+                            projectId={projectId}
+                            measures={measures}
+                            rows={rows}
+                            columns={columns}
+                            totals={totals}
+                            totalsEditAllowed={true}
+                            pageSize={20}
+                            sortBy={sortBy}
+                            drillableItems={drillableItems}
+                            onFiredDrillEvent={this.onDrill}
+                            config={({
+                                menu: {
+                                    aggregations: true
+                                }
+                            })}
+                        />
+                    </div>
+
                 </div>
+
             </div>
         );
     }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,16 @@
 const proxy = require('http-proxy-middleware');
 
+const backend = 'staging.intgdc.com';
+// const backend = 'ppl-mini2.na.intgdc.com';
+
 module.exports = function (app) {
      app.use(proxy("/gdc", {
          "changeOrigin": true,
          "cookieDomainRewrite": "localhost",
          "secure": false,
-         "target": "https://staging3.intgdc.com",
+         "target": `https://${backend}`,
          "headers": {
-             "host": "staging3.intgdc.com",
+             "host": backend,
              "origin": null
          },
          "onProxyReq": function(proxyReq, req, res) {
@@ -17,11 +20,11 @@ module.exports = function (app) {
      app.use(proxy("/*.html", {
          "changeOrigin": true,
          "secure": false,
-         "target": "https://staging3.intgdc.com"
+         "target": `https://${backend}`
      }));
      app.use(proxy("/packages/*.{js,css}", {
          "changeOrigin": true,
          "secure": false,
-         "target": "https://staging3.intgdc.com"
+         "target": `https://${backend}`
      }));
  };
